724b65477089f84cd7d6caf0e5d5a23f2be2a0d8
Harry Gardiner <gardiner_harry@yahoo.com>
2021-02-20 12:49:38 +0000

    Created learning points file to be updated as I go. JSON file accessed and converted to PHP array. Array passed to Banana class. Able to get array item and values

--- app/Bananas.php
+++ app/Bananas.php
@@ -2,4 +2,31 @@
 
 require __DIR__ . "/../vendor/autoload.php";
 
-dump("Hello World");
+class Bananas
+{
+
+    private $journey_data;
+
+    public function __construct($json)
+    {
+        $this->journey_data = $json;
+    }
+
+    public function getJourneyData()
+    {
+        return $this->journey_data;
+    }
+
+}
+
+// Read JSON file
+$json = file_get_contents('./testdata.json');
+
+//Decode JSON into PHP array
+$json_data = json_decode($json, true);
+
+$Banana = new Bananas($json_data);
+dump($Banana->getJourneyData());
+dump($Banana->getJourneyData()[0]);
+dump($Banana->getJourneyData()[0]["from"]);
+
---
+++ learning_points.md
@@ -0,0 +1,3 @@
+- Arrays in PHP will also be converted into JSON when using the PHP function `json_encode()`:
+
+- PHP >= 5.2.0 features a function, `json_decode()`, that decodes a JSON string into a PHP variable. By default it returns an object. The second parameter accepts a boolean that when set as true, tells it to return the objects as associative arrays.
